1495c0e00c5a7a009d603ab3788bfcc933f7039a
uic19bca1130 <128672958+uic19bca1130@users.noreply.github.com>
2023-03-31 14:10:56 +0530

    masters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
 
-function App() {
+function QRCodeGenerator() {
+  const [formData, setFormData] = useState({
+    latitude: '30.6991158',
+    longitude: '76.6914105',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Generate QR Code</h1>
+      <form>
+        <label>
+          Latitude:
+          <input type="text" name="latitude" value={formData.latitude} onChange={handleChange} />
+        </label>
+        <label>
+          Longitude:
+          <input type="text" name="longitude" value={formData.longitude} onChange={handleChange} />
+        </label>
+      </form>
+      <div className="qr-code-container">
+        <QRCode
+          value={`geo:${formData.latitude},${formData.longitude}`}
+          fgColor="#AF0000"
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default QRCodeGenerator;
